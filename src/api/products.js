c8a3054ec45e98000221cf93432723536287a7e0
anhtuan0302 <anhtuan030203@gmail.com>
2023-10-29 16:08:22 +0700

    Config domain

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://backend.tuanngoanh.id.vn',
 }); 
 
 const getProducts = async () => { 
